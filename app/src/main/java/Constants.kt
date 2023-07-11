b28752cdca3bf44a6e2a4d410d598f8bf1ce7f31
Pavel Karpau <darthsantalaze@gmail.com>
2023-07-11 15:25:30 +0300

    Implements request authorisation to access data https://trello.com/c/v0p9I6an

---
+++ app/src/main/java/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.music_app
+
+const val SPOTIFY_CLIENT_ID = "67f6367abcd3497db54043591d9e7c9f"
+const val CODE = "code"
+const val SCOPE = "user-read-private user-read-email"
+const val REDIRECT_URL = "http://localhost:3000"
--- app/src/main/java/com/example/music_app/MainActivity.kt
+++ app/src/main/java/com/example/music_app/MainActivity.kt
@@ -1,46 +1,37 @@
 package com.example.music_app
 
+
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.music_app.ui.theme.MusicappTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MusicappTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.activity_main)
+        val webView = findViewById<View>(R.id.webview) as WebView
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&response_type=${CODE}&scope=${SCOPE}&redirect_uri=${REDIRECT_URL}")
+        webView.webViewClient = AppWebViewClient()
+    }
+
+    private inner class AppWebViewClient: WebViewClient() {
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            val uri = Uri.parse(url)
+            val state = uri.getQueryParameter("state")
+            val error = uri.getQueryParameter("error")
+            val code = uri.getQueryParameter("code")
+            if (error == null && code != null) {
+
             }
         }
     }
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MusicappTheme {
-        Greeting("Android")
-    }
-}
