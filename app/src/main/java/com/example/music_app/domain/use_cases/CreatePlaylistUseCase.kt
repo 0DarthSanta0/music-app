55ae5145d750baf082213a5866a1cbe8a3985b9a
Pavel Karpau <darthsantalaze@gmail.com>
2023-08-10 14:17:37 +0300

    Add logic to CreatePlaylistUseCase https://trello.com/c/iTlNQIid

--- app/src/main/java/com/example/music_app/domain/use_cases/CreatePlaylistUseCase.kt
+++ app/src/main/java/com/example/music_app/domain/use_cases/CreatePlaylistUseCase.kt
@@ -1,4 +1,13 @@
 package com.example.music_app.domain.use_cases
 
-class CreatePlaylistUseCase {
+import com.example.music_app.domain.repositories.PlaylistsRepository
+
+class CreatePlaylistUseCase(
+    private val playlistsRepository: PlaylistsRepository
+) {
+    suspend operator fun invoke(name: String, description: String) =
+        playlistsRepository.createPlaylist(
+            name = name,
+            description = description
+        )
 }
